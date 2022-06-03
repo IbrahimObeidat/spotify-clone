2106998e8cec84ca9daf6553d1d69fcba9f40072
Ibrahim Obeidat <barhoomob@gmail.com>
2022-06-03 23:17:29 +0300

    Load front-end with access and refresh tokens
    As URL queries

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
+  // to get the access and refresh tokens from url queries
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlPrams = new URLSearchParams(queryString);
+    const access_token = urlPrams.get("access_token");
+    const refresh_token = urlPrams.get("refresh_token");
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
--- index.js
+++ index.js
@@ -63,20 +63,19 @@ app.get("/callback", (req, res) => {
   })
     .then((response) => {
       if (response.status === 200) {
-        const { access_token, token_type } = response.data;
+        const { access_token, refresh_token } = response.data;
 
-        axios
-          .get("https://api.spotify.com/v1/me", {
-            headers: {
-              Authorization: `${token_type} ${access_token}`,
-            },
-          })
-          .then((response) => {
-            res.send(response.data);
-          })
-          .catch((err) => res.send(err));
+        const queryParams = querystring.stringify({
+          access_token,
+          refresh_token,
+        });
+        res.redirect(`http://localhost:3000/?${queryParams}`);
       } else {
-        res.send(response);
+        res.redirect(
+          `/?${querystring.stringify({
+            error: "invalid_token",
+          })}`
+        );
       }
     })
     .catch((err) => res.send(err));
