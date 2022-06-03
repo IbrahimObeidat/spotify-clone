0498f6765a0cb61296d4c0927b7637d5ef5e2dae
Ibrahim Obeidat <barhoomob@gmail.com>
2022-06-04 02:40:52 +0300

    Use local storage to persist login state

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
-import { useEffect } from "react";
-import logo from "./logo.svg";
+import { useEffect, useState } from "react";
+import { accessToken, logout } from "./api";
 import "./App.css";
 
 const App = () => {
-  // to get the access and refresh tokens from url queries
+  const [token, setToken] = useState(null);
+
   useEffect(() => {
-    const queryString = window.location.search;
-    const urlPrams = new URLSearchParams(queryString);
-    const access_token = urlPrams.get("access_token");
-    const refresh_token = urlPrams.get("refresh_token");
+    setToken(accessToken);
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a href="http://localhost:8888/login">Login to Spotify</a>
+        {!token ? (
+          <a href="http://localhost:8888/login">Login to Spotify</a>
+        ) : (
+          <>
+            <h1>Logged in!</h1>
+            <button onClick={logout}>Logout</button>
+          </>
+        )}
       </header>
     </div>
   );
